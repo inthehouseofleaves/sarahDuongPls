97d77ae2a65d6b58b50f586a397b1ac199aee396
Sarah <sarahmichelleduong@gmail.com>
2020-09-08 02:07:42 -0400

    more stuff, including media queries

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
+// BEFORE ANYTHING: CREATE YOUR RESPONSES???
 
 
 const dateSpot = [
     
 ]
 
-// 1. Listener event?
+// 1. DOCUMENT READY
+
+$(document).ready(function(){
+
+
+// 2. CREATE AN EVENT LISTENER FOR FORM SUBMIT
+
+$("form").on("submit", function(event){
+
+    // 3. PREVENT THE FORM FROM REFRESHING
+
+    event.preventDefault();
+
+    // 4. MAKE YOUR BUTTON WORK
+    $("button").on()
+    console.log("You are submitting your form!")
+
+
+
+})
+
+
+
+
+
+
+
+
+
+})
